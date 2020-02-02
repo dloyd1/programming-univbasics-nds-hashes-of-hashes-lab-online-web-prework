83af0eebf2af13f7e807d651a71dbca10926bcce
Daniel Loyd <dhloyd@icloud.com>
2020-02-02 14:10:32 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,12 +31,13 @@ PORTION_4 = {
 }
 
 def naming_system
+a_system = Hash.new
 a_system = {
    PORTION_1,
    PORTION_2,
    PORTION_3,
    PORTION_4
-}
+  }
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
 a_system
